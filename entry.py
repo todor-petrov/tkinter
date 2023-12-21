f54b88cb50fefaf396d33ec0a859973269012813
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-12-21 14:20:49 +0200

    Add files via upload

---
+++ entry.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+root = Tk()
+
+e = Entry(root, width=50, borderwidth=5)
+e.pack()
+e.insert(0, 'Enter Your Name')
+
+def myClick():
+    hello = f'Hello, {e.get()}!'
+    myLabel = Label(root, text=hello)
+    myLabel.pack()
+
+myButton = Button(root, text='Enter Your Name', command=myClick)
+
+myButton.pack()
+
+root.mainloop()
